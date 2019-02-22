2e3a5f9e0167ee67779e845af739576f037b57d8
Jinqi Song <songjinqijess@gmail.com>
2019-02-21 20:12:22 -0800

    we implement the big 4s

--- tempelate.h
+++ tempelate.h
@@ -43,3 +43,74 @@ namespace Pic10b{
         vector<T> operator*(T a );
         vector<T> operator+(const vector& );
     }; // end of class
+	
+	
+	
+    /** ************************* THE BIG 4 ************************* **/
+    
+    //default constructor
+    template<typename T>
+    vector<T>::vector()
+    : the_data(nullptr), the_size(0), the_capacity(INIT_CAP) {
+        std::cout<<"xxxxxxx Default Constructor Called xxxxxxx"<<std::endl;
+        the_data = new T[the_capacity];
+    }
+    
+    
+    //copy constructor
+    template<typename T>
+    vector<T>::vector( const vector<T>& source )
+    : the_data(nullptr), the_size(source.the_size),
+    the_capacity(source.the_capacity) {
+        std::cout<<"xxxxxxx Copy Constructor Called xxxxxxx"<<std::endl;
+        
+        the_data = new T[the_capacity];
+        
+        // Deep copy of internal array
+        for ( int i = 0 ; i < the_size ; ++i ){
+            the_data[i] = source.the_data[i];
+        }
+        
+    }
+    
+    //assginment operator
+    template<typename T>
+    vector<T>& vector<T>::operator=( const vector<T>& rhs ) {
+         std::cout<<"xxxxxxx Assignment Operator xxxxxxx"<<std::endl;
+        if ( this != &rhs ) {     // x=x
+            // Release old memory and request more
+            delete[] the_data;
+            the_data = new T[rhs.the_capacity];
+            
+            // Shallow copy non-pointers
+            the_size = rhs.the_size;
+            the_capacity = rhs.the_capacity;
+            
+            // Deep copy internal array
+            for ( int i = 0 ; i < the_size ; ++i )
+                the_data[i] = rhs.the_data[i];
+        }
+       
+        return *this;
+    }
+    
+    //destructor
+    template<typename T>
+    vector<T>::~vector<T>(){
+         std::cout<<"xxxxxxx Dustructor Called xxxxxxx"<<std::endl;
+        delete[] the_data;
+       
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
